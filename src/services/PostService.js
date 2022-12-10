a8e3c80584d924dbc50f8cc1374d0d1854a71836
calvinthurst <65094705+calvinthurst@users.noreply.github.com>
2022-12-10 11:34:26 -0700

    second commit

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -7,18 +7,34 @@ class PostService {
   async getPosts() {
     const res = await api.get('api/posts')
     AppState.posts = res.data.posts.map(p => new Post(p))
-    logger.log('the Posts sir', AppState.posts)
-  }
-  async getPostByCreator(profileId) {
-    const res = await api.get(`/api/posts/`, { params: { creatorId: profileId } })
-    AppState.posts = res.data.posts.map(p => new Post(p))
-    logger.log('appstate post by profile', AppState.posts)
+    let page = res.data.page.slice(0, 1)
+    AppState.page = Number(page)
+    AppState.maxPage = res.data.totalPages
+
+    // logger.log('the Posts sir', AppState.posts)
   }
 
+
   async createPost(postData) {
-    const res = await api.post('api/posts')
+    const res = await api.post('api/posts', postData)
     AppState.posts.unshift(new Post(res.data))
-    logger.log(res.data)
+    // logger.log(res.data)
+  }
+
+  async removePost(postId) {
+    const res = await api.delete('api/posts/' + postId)
+    let postIndex = AppState.posts.findIndex(p => p.id == postId)
+    if (postIndex >= 0) {
+      AppState.posts.splice(postIndex, 1)
+    }
+    // logger.log(res.data)
+  }
+  async changePage(page) {
+    const res = await api.get('/api/posts?page=' + page)
+    AppState.posts = res.data.posts.map(p => new Post(p))
+    let pageNum = res.data.page.slice(0, 1)
+    AppState.page = Number(pageNum)
+    AppState.maxPage = res.data.totalPages
   }
 }
 export const postService = new PostService()
